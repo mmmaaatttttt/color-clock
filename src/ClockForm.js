2a808358994850f571fabdf5ef3b1219fc3a4dc0
Matt Lane <lane.matthew@gmail.com>
2020-07-03 16:11:03 -0700

    add ColorClock and ClockForm components

---
+++ src/ClockForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ClockForm(props) {
+  return <div>ClockForm</div>;
+}
+
+export default ClockForm;
---
+++ src/ColorClock.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ClockForm from "./ClockForm";
+
+function ColorClock(props) {
+  return (
+    <div>
+      <h1>ColorClock</h1>
+      <ClockForm />
+    </div>
+  );
+}
+
+export default ColorClock;
