50df7834dfefd1ecb7aa5547b971a40d73d2154a
Matt Lane <lane.matthew@gmail.com>
2020-07-03 17:14:41 -0700

    add basic timer for clock in ColorClock

--- src/ColorClock.js
+++ src/ColorClock.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function ColorClock({ clockData }) {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  })
+
   return (
     <div>
       <h4>ColorClock</h4>
       <pre>{JSON.stringify(clockData, null, 2)}</pre>
+      <p>{time.toLocaleTimeString()}</p>
     </div>
   );
 }
