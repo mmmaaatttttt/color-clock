cb4e3cea55104f1477eed35d661ab3c4f8422445
Matt Lane <lane.matthew@gmail.com>
2020-07-04 09:06:27 -0700

    add timeToNumber utility and comments

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,29 @@
+/**
+ * Capitalizes the first letter in a string.
+ *
+ * @param {String} str - String to be capitalized.
+ * @returns {String} Returns the capitalized string.
+ */
 export function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1);
+}
+
+/**
+ * Converts a time of the form "HH:MM" or "HH:MM:SS"
+ * to a number in seconds.
+ *
+ * @param {String} time - Time in one of the above formats.
+ * @returns {Number} Returns a number in seconds.
+ */
+export function timeToNumber(time) {
+  const secondsPerHour = 3600;
+  const secondsPerMinute = secondsPerHour / 60;
+  let timeArr = time.split(":").map(Number);
+
+  // if no seconds provided, default to 0 seconds
+  if (timeArr.length === 2) timeArr.push(0);
+
+  return (
+    timeArr[0] * secondsPerHour + timeArr[1] * secondsPerMinute + timeArr[2]
+  );
 }
