9c12a1a37013ef58d52ffae97635f0223cccd51c
Matt Lane <lane.matthew@gmail.com>
2020-07-04 16:17:28 -0700

    refactor ColorClock to use time helpers

--- src/ColorClock.js
+++ src/ColorClock.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { scaleLinear } from "d3-scale";
-import { timeToNumber } from "./utils";
+import { timeToNumber, getTime, howLongAgo } from "./utils";
 import "./ColorClock.css";
 
 function ColorClock({
@@ -11,48 +11,44 @@ function ColorClock({
   warningColor,
   endColor
 }) {
-  const [time, setTime] = useState(new Date());
+  const [{ timeForDisplay, timeForComparison }, setTime] = useState(getTime());
   const [color, setColor] = useState(startColor);
-  const timeStrDisplay = time.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
-  const timeStrInternal = time.toLocaleTimeString("en-US", { hour12: false });
 
   useEffect(() => {
     const timerId = setInterval(() => {
-      const newTime = new Date();
-      const newTimeStr = newTime.toLocaleTimeString("en-US", { hour12: false });
       const timeArr = [startTime, warningTime, endTime].map(timeToNumber);
       const colorArr = [startColor, warningColor, endColor];
       const colorScale = scaleLinear().domain(timeArr).range(colorArr);
-      setTime(new Date());
-      setColor(colorScale(timeToNumber(newTimeStr)));
+
+      const newTime = getTime();
+      setTime(newTime);
+      setColor(colorScale(timeToNumber(newTime.timeForComparison)));
     }, 1000);
 
     return () => clearTimeout(timerId);
   }, [startTime, startColor, warningTime, warningColor, endTime, endColor]);
 
-  if (timeStrInternal < startTime)
+  if (timeForComparison < startTime)
     return (
       <div>
         <p>This clock will start at {startTime}.</p>
-        <p>Current time: {timeStrDisplay}.</p>
+        <p>Current time: {timeForDisplay}.</p>
       </div>
     );
 
-  if (timeStrInternal > endTime) {
+  if (timeForComparison > endTime) {
     return (
       <div className="ColorClock" style={{ backgroundColor: endColor }}>
-        <p>Time's up! This clock ended at {endTime}.</p>
-        <p>Current time: {timeStrDisplay}.</p>
+        <h1>Time's Up!</h1>
+        <h4>This clock ended {howLongAgo(endTime)}.</h4>
+        <h4>Current time: {timeForDisplay}.</h4>
       </div>
     );
   }
 
   return (
     <div className="ColorClock" style={{ backgroundColor: color }}>
-      <h1>{timeStrDisplay}</h1>
+      <h1>{timeForDisplay}</h1>
     </div>
   );
 }
