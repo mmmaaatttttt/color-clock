f13baba8a0a83072389ca930887baed9d130dedf
Matt Lane <lane.matthew@gmail.com>
2020-07-04 16:17:08 -0700

    add more time manipulation helpers

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 /**
  * Capitalizes the first letter in a string.
  *
@@ -26,4 +28,40 @@ export function timeToNumber(time) {
   return (
     timeArr[0] * secondsPerHour + timeArr[1] * secondsPerMinute + timeArr[2]
   );
+}
+
+/**
+ * Gets the current time; returns an object with the time in two formats.
+ *
+ * @returns {Object} an object with two formats for the time:
+ *  timeForDisplay: h:mm A (e.g. 3:16 PM)
+ *  timeForColor: H:mm:ss (e.g. 15:16:23)
+ *
+ * The first formatting is for the view,
+ * the second is for determining the background color and general comparison
+ * (since start and end times are in 24 hour formatting).
+ */
+export function getTime() {
+  const newMoment = moment();
+
+  return {
+    timeForDisplay: newMoment.format("LT"),
+    timeForComparison: newMoment.format("H:mm:ss")
+  };
+}
+
+/**
+ * Generate a human readable description of how long ago the input time was
+ * (for times within the same day).
+ *
+ * @param {String} time - 24 hour time as a string, e.g. "15:16"
+ * @returns {String} English description of how long ago the time was.
+ */
+export function howLongAgo(time) {
+  const [hour, minutes] = time.split(":").map(Number);
+  return moment()
+    .startOf("day")
+    .add(hour, "hours")
+    .add(minutes, "minutes")
+    .fromNow();
 }
