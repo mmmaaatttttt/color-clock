a53f10c19a26060433856053f6e7b353a2eebd8d
Matt Lane <lane.matthew@gmail.com>
2020-07-04 09:07:50 -0700

    add new draft of ColorClock component

--- src/ColorClock.js
+++ src/ColorClock.js
@@ -1,21 +1,58 @@
 import React, { useState, useEffect } from "react";
+import { scaleLinear } from "d3-scale";
+import { timeToNumber } from "./utils";
+import "./ColorClock.css";
 
-function ColorClock({ clockData }) {
+function ColorClock({
+  startTime,
+  warningTime,
+  endTime,
+  startColor,
+  warningColor,
+  endColor
+}) {
   const [time, setTime] = useState(new Date());
+  const [color, setColor] = useState(startColor);
+  const timeStrDisplay = time.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+  const timeStrInternal = time.toLocaleTimeString("en-US", { hour12: false });
 
   useEffect(() => {
     const timerId = setInterval(() => {
+      const newTime = new Date();
+      const newTimeStr = newTime.toLocaleTimeString("en-US", { hour12: false });
+      const timeArr = [startTime, warningTime, endTime].map(timeToNumber);
+      const colorArr = [startColor, warningColor, endColor];
+      const colorScale = scaleLinear().domain(timeArr).range(colorArr);
       setTime(new Date());
+      setColor(colorScale(timeToNumber(newTimeStr)));
     }, 1000);
 
     return () => clearTimeout(timerId);
-  })
+  });
+
+  if (timeStrInternal < startTime)
+    return (
+      <div>
+        <p>This clock will start at {startTime}.</p>
+        <p>Current time: {timeStrDisplay}.</p>
+      </div>
+    );
+
+  if (timeStrInternal > endTime) {
+    return (
+      <div>
+        <p>Time's up! This clock ended at {endTime}.</p>
+        <p>Current time: {timeStrDisplay}.</p>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h4>ColorClock</h4>
-      <pre>{JSON.stringify(clockData, null, 2)}</pre>
-      <p>{time.toLocaleTimeString()}</p>
+    <div className="ColorClock" style={{ backgroundColor: color }}>
+      <h1>{timeStrDisplay}</h1>
     </div>
   );
 }
