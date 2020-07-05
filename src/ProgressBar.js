5193ce40a71277a06efd971e7451ac57cca673d8
Matt Lane <lane.matthew@gmail.com>
2020-07-04 21:32:40 -0700

    add caret to progress bar

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import "./ProgressBar.css";
 
-function ProgressBar({ gradientPercentage, color1, color2, color3 }) {
+function ProgressBar({
+  gradientPercentage,
+  caretPercentage,
+  color1,
+  color2,
+  color3
+}) {
   const background = `linear-gradient(
     90deg, ${color1} 0%,${color2} ${gradientPercentage}%, ${color3} 100%
   )`;
+  console.log("%%%", caretPercentage);
   return (
     <div className="ProgressBar">
-      <div className="ProgressBar--bar" style={{ background }}></div>
+      <div className="ProgressBar--bar" style={{ background }}>
+        <FontAwesomeIcon
+          icon={faCaretUp}
+          size="2x"
+          className="ProgressBar--caret"
+          style={{ left: `${caretPercentage}%` }}
+        />
+      </div>
     </div>
   );
 }
