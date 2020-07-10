a267c5237cf93b6b6c3c84d3b44a857b1746a7f8
Matt Lane <lane.matthew@gmail.com>
2020-07-09 23:35:21 -0700

    fix snapshot tests for ColorClock

--- src/components/ColorClock/ColorClock.test.js
+++ src/components/ColorClock/ColorClock.test.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import MockDate from "mockdate";
 import ColorClock from "./";
+import { makeTimeStamp } from "../../utils/testUtils";
 
 describe("basic ColorClock tests", function () {
   it("renders without crashing", function () {
     render(<ColorClock />);
   });
 
-  it("matches snapshot", function () {
+  it("matches snapshot before the clock is active", function () {
+    MockDate.set(makeTimeStamp("08:00:00"));
     const { asFragment } = render(<ColorClock />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("matches snapshot while the clock is active", function () {
+    MockDate.set(makeTimeStamp("10:00:00"));
+    const { asFragment } = render(<ColorClock />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("matches snapshot after the clock is active", function () {
+    MockDate.set(makeTimeStamp("14:00:00"));
+    const { asFragment } = render(<ColorClock />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  afterEach(function() {
+    MockDate.reset()
   });
 });
