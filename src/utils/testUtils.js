8580960a18c89aa804e42d538cd1a34fe883c5fb
Matt Lane <lane.matthew@gmail.com>
2020-07-17 21:57:38 -0700

    refactor time mock helper

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,9 +1,24 @@
+import MockDate from "mockdate";
+
 /**
  * Create a timestamp out of a 24 hour time string.
  * Used in conjunction with mocking the Date constructor via MockDate.
  *
  * @param {String} time - 24 hour time in HH:mm:ss format (e.g. 15:16:23)
  */
-export function makeTimeStamp(time) {
+function makeTimeStamp(time) {
   return `Sun Jul 05 2020 ${time} GMT-0700`;
 }
+
+/**
+ * Abstraction around MockDate that lets us mock dates by providing
+ * only times, not dates.
+ */
+export const timeMock = {
+  set(time) {
+    MockDate.set(makeTimeStamp(time));
+  },
+  reset() {
+    MockDate.reset();
+  }
+};
