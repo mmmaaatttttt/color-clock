e27dd3918ed5eb8d7fee2089f16fe1ac5cc2cfc6
Matt Lane <lane.matthew@gmail.com>
2020-07-17 21:54:45 -0700

    refactor ColorClock tests

--- src/components/ColorClock/ColorClock.test.js
+++ src/components/ColorClock/ColorClock.test.js
@@ -1,33 +1,34 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import MockDate from "mockdate";
 import ColorClock from "./";
-import { makeTimeStamp } from "../../utils/testUtils";
+import { timeMock } from "../../utils/testUtils";
 
-describe("basic ColorClock tests", function () {
-  it("renders without crashing", function () {
-    render(<ColorClock />);
-  });
+describe("ColorClock component", function () {
+  describe("basic tests", function () {
+    it("renders without crashing", function () {
+      render(<ColorClock />);
+    });
 
-  it("matches snapshot before the clock is active", function () {
-    MockDate.set(makeTimeStamp("08:00:00"));
-    const { asFragment } = render(<ColorClock />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+    it("matches snapshot before the clock is active", function () {
+      timeMock.set("08:00:00");
+      const { asFragment } = render(<ColorClock />);
+      expect(asFragment()).toMatchSnapshot();
+    });
 
-  it("matches snapshot while the clock is active", function () {
-    MockDate.set(makeTimeStamp("10:00:00"));
-    const { asFragment } = render(<ColorClock />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+    it("matches snapshot while the clock is active", function () {
+      timeMock.set("10:00:00");
+      const { asFragment } = render(<ColorClock />);
+      expect(asFragment()).toMatchSnapshot();
+    });
 
-  it("matches snapshot after the clock is active", function () {
-    MockDate.set(makeTimeStamp("14:00:00"));
-    const { asFragment } = render(<ColorClock />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+    it("matches snapshot after the clock is active", function () {
+      timeMock.set("14:00:00");
+      const { asFragment } = render(<ColorClock />);
+      expect(asFragment()).toMatchSnapshot();
+    });
 
-  afterEach(function() {
-    MockDate.reset()
+    afterEach(function () {
+      timeMock.reset();
+    });
   });
 });
