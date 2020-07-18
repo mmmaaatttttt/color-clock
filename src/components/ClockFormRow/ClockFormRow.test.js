3f4ff1769abd44635807eb7d84d1c759ff665085
Matt Lane <lane.matthew@gmail.com>
2020-07-17 22:06:06 -0700

    fix broken ClockFormRow tests

--- src/components/ClockFormRow/ClockFormRow.test.js
+++ src/components/ClockFormRow/ClockFormRow.test.js
@@ -1,19 +1,45 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ClockFormRow from "./";
+import FormContext from "../../contexts/FormContext";
 
-describe("basic ClockFormRow tests", function () {
-  it("renders without crashing", function () {
-    render(<ClockFormRow />);
-  });
+describe("ClockFormRow component", function () {
+  describe("basic tests", function () {
+    const contextValue = {
+      formData: { startTime: "", startColor: "#ff0000" },
+      errors: {},
+      handleChange: () => {}
+    };
 
-  it("matches snapshot", function () {
-    const { asFragment } = render(<ClockFormRow />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+    it("renders without crashing", function () {
+      render(
+        <FormContext.Provider value={contextValue}>
+          <ClockFormRow />
+        </FormContext.Provider>
+      );
+    });
 
-  it("matches snapshot with errors", function () {
-    const { asFragment } = render(<ClockFormRow error="this is an error." />);
-    expect(asFragment()).toMatchSnapshot();
+    it("matches snapshot", function () {
+      const { asFragment } = render(
+        <FormContext.Provider value={contextValue}>
+          <ClockFormRow />
+        </FormContext.Provider>
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("matches snapshot with errors", function () {
+      const { asFragment } = render(
+        <FormContext.Provider
+          value={{
+            ...contextValue,
+            errors: { startTime: "This is an error." }
+          }}
+        >
+          <ClockFormRow />
+        </FormContext.Provider>
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 });
