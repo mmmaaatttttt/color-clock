709026d13807098826c7e6fa4e4fd8dd62092a45
Matt Lane <lane.matthew@gmail.com>
2020-07-19 09:12:39 -0700

    add test for desired redirect behavior

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,27 +2,36 @@ import React from "react";
 import App from "./";
 import { renderWithRouter, timeMock } from "../../utils/testUtils";
 
-describe("basic App tests", function () {
-  it("renders without crashing", function () {
-    renderWithRouter(<App />);
-  });
-
-  it("matches snapshot for the form", function () {
-    const { asFragment } = renderWithRouter(<App />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  it("matches snapshot for about", function () {
-    const { asFragment } = renderWithRouter(<App />, { route: "/about" });
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  it("matches snapshot for a valid clock", function () {
-    timeMock.set("10:00");
-    const { asFragment } = renderWithRouter(<App />, {
-      route: "/clocks/3tH42XskKAkY7NYHrms"
+describe("App component", function () {
+  describe("basic tests", function () {
+    it("renders without crashing", function () {
+      renderWithRouter(<App />);
     });
-    expect(asFragment()).toMatchSnapshot();
-    timeMock.reset();
+
+    it("matches snapshot for the form", function () {
+      const { asFragment } = renderWithRouter(<App />);
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("matches snapshot for about", function () {
+      const { asFragment } = renderWithRouter(<App />, { route: "/about" });
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("matches snapshot for a valid clock", function () {
+      timeMock.set("10:00");
+      const { asFragment } = renderWithRouter(<App />, {
+        route: "/clocks/3tH42XskKAkY7NYHrms"
+      });
+      expect(asFragment()).toMatchSnapshot();
+      timeMock.reset();
+    });
+
+    it("redirects to the form with an invalid url", function() {
+      const { getByText } = renderWithRouter(<App />, {
+        route: "/nope"
+      });
+      expect(getByText("When do you want the clock to start?")).toBeInTheDocument();
+    })
   });
 });
