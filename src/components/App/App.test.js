68cc7a89bd30c12aecfc5e70a4dfd1af33b6f7b5
Matt Lane <lane.matthew@gmail.com>
2020-07-18 21:18:00 -0700

    update app tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,28 @@
 import React from "react";
 import App from "./";
-import { renderWithRouter } from "../../utils/testUtils";
+import { renderWithRouter, timeMock } from "../../utils/testUtils";
 
 describe("basic App tests", function () {
   it("renders without crashing", function () {
     renderWithRouter(<App />);
   });
 
-  it("matches snapshot", function () {
+  it("matches snapshot for the form", function () {
     const { asFragment } = renderWithRouter(<App />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("matches snapshot for about", function () {
+    const { asFragment } = renderWithRouter(<App />, { route: "/about" });
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("matches snapshot for a valid clock", function () {
+    timeMock.set("10:00");
+    const { asFragment } = renderWithRouter(<App />, {
+      route: "/clocks/3tH42XskKAkY7NYHrms"
+    });
+    expect(asFragment()).toMatchSnapshot();
+    timeMock.reset();
   });
 });
