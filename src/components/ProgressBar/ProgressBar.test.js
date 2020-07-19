e48210bc3be16de722ee6c7aaed6a3d444559696
Matt Lane <lane.matthew@gmail.com>
2020-07-18 20:31:06 -0700

    update progressbar tests

--- src/components/ProgressBar/ProgressBar.test.js
+++ src/components/ProgressBar/ProgressBar.test.js
@@ -2,13 +2,22 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ProgressBar from "./";
 
-describe("basic ProgressBar tests", function () {
-  it("renders without crashing", function () {
-    render(<ProgressBar />);
-  });
+describe("ProgressBar", function () {
+  describe("basic tests", function () {
+    it("renders without crashing", function () {
+      render(<ProgressBar />);
+    });
 
-  it("matches snapshot", function () {
-    const { asFragment } = render(<ProgressBar />);
-    expect(asFragment()).toMatchSnapshot();
+    it("matches snapshot", function () {
+      const { asFragment } = render(<ProgressBar />);
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("matches snapshot with different props", function () {
+      const { asFragment } = render(
+        <ProgressBar hidden colors={["#fff0f0", "#ff0f0f", "#ff00ff"]} />
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 });
