ea022c052d4ba6e6b47afe47e881fcf188e13288
Matt Lane <lane.matthew@gmail.com>
2020-07-18 21:01:41 -0700

    update About tests

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -1,14 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, waitForElementToBeRemoved } from "@testing-library/react";
+import App from "../App";
 import About from "./";
+import { renderWithRouter } from "../../utils/testUtils";
 
-describe("basic About tests", function () {
-  it("renders without crashing", function () {
-    render(<About />);
+describe("About", function () {
+  describe("basic tests", function () {
+    it("renders without crashing", function () {
+      renderWithRouter(<About />);
+    });
+
+    it("matches snapshot", function () {
+      const { asFragment } = renderWithRouter(<About />);
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 
-  it("matches snapshot", function () {
-    const { asFragment } = render(<About />);
-    expect(asFragment()).toMatchSnapshot();
+  describe("navigation", function () {
+    it("goes back to the form when clicking the link", async function () {
+      const { getByText } = renderWithRouter(<App />, {
+        route: "/about"
+      });
+
+      const startText = "When do you want the clock to start?";
+      const linkText = "Go back";
+
+      const backLink = getByText(linkText);
+      fireEvent.click(backLink);
+
+      await waitForElementToBeRemoved(() => getByText(linkText));
+
+      expect(getByText(startText)).toBeInTheDocument();
+    });
   });
 });
