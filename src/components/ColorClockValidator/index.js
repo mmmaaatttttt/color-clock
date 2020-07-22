011ca3515e9b7f3174914c1684cffac68c2e1c8e
Matt Lane <lane.matthew@gmail.com>
2020-07-21 19:16:37 -0700

    move validate function outside of ColorClockValidator

--- src/components/ColorClockValidator/index.js
+++ src/components/ColorClockValidator/index.js
@@ -3,31 +3,33 @@ import { Redirect, useParams } from "react-router-dom";
 import { decodeFromUrl } from "../../utils/routes";
 import ColorClock from "../ColorClock";
 
+const validateId = (clockId) => {
+  try {
+    const { colors, times } = decodeFromUrl(clockId);
+    if (colors.length !== 3 || times.length !== 3) {
+      throw new Error(
+        "Color clock requires three colors and three times.\n" +
+          `Colors received: ${colors.length}.\n` +
+          `Times received: ${times.length}.`
+      );
+    }
+    return { colors, times };
+  } catch (err) {
+    console.warn(
+      `Route id ${clockId} is invalid. Redirecting to '/'. Original error below:`
+    );
+    console.warn(err);
+    return false;
+  }
+};
+
 function ColorClockValidator() {
   const { clockId } = useParams();
 
-  const validateId = () => {
-    if (!clockId) return false;
-    try {
-      const { colors, times } = decodeFromUrl(clockId);
-      if (colors.length !== 3 || times.length !== 3) {
-        throw new Error(
-          "Color clock requires three colors and three times.\n" +
-            `Colors received: ${colors.length}.\n` +
-            `Times received: ${times.length}.`
-        );
-      }
-      return { colors, times };
-    } catch (err) {
-      console.warn(
-        `Route id ${clockId} is invalid. Redirecting to '/'. Original error below:`
-      );
-      console.warn(err);
-      return false;
-    }
-  };
+  // exit without redirecting if mid transition
+  if (!clockId) return null;
 
-  let validatedObj = validateId();
+  let validatedObj = validateId(clockId);
 
   if (!validatedObj) {
     return <Redirect to={{ pathname: "/", state: { direction: "up" } }} />;
